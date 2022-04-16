9f1535dde26d7be9bf30dd1e43682a92d3444eeb
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2022-04-16 21:55:49 +0530

    Leetcode 538 | Medium

--- LeetCode/Convert BST to Greater Tree.py
+++ LeetCode/Convert BST to Greater Tree.py
@@ -1,4 +1,5 @@
 
+# Method 1
 class Solution:
     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         def dfs(node:Optional[TreeNode],val:int)->int:
@@ -7,4 +8,19 @@ class Solution:
             return dfs(node.left,node.val)
         sum=dfs(root,0)
         return root
-      
+
+# Method 2
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        curSum = 0
+        def dfs(node):
+            if not node:
+                return
+            nonlocal curSum
+            dfs(node.right)
+            temp = node.val
+            node.val += curSum
+            curSum += temp
+            dfs(node.left)
+        dfs(root)
+        return root
