7071e525e83835191eeac1b4a9256fec5ae0a4e8
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2022-05-02 19:02:19 +0530

    Party in Town | Medium

---
+++ GeeksForGeeks/Party in Town.py
@@ -0,0 +1,19 @@
+
+#User function Template for python3
+
+class Solution:
+    def partyHouse(self, N, adj):
+        # code here
+        def dfs(curr,prev,dep):
+            maxdep[0] = max(maxdep[0],dep)
+            for edge in adj[curr]:
+                if edge != prev:
+                    dfs(edge,curr,dep+1)
+        
+        ans = 999999999
+        for i in range(1,N+1):
+            maxdep = [0]
+            dfs(i,-1,0)
+            ans = min(ans,maxdep[0])
+        return ans
+      
