55fa6b427626fba3d527bc519a816ae50bcaa8a3
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2022-05-31 23:01:35 +0530

    Leetcode 1461 | Medium

---
+++ May LeetCoding Challenge 2022/Check If a String Contains All Binary Codes of Size K.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        
+        hashset = set()
+        
+        for i in range(len(s) - k+1):
+            hashset.add(s[i:i+k])
+            
+        return len(hashset) == 2 ** k
+      
