cea15b67c5990f9e09f1d98e92cfc08f5b361ba3
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2022-06-20 23:35:02 +0530

    Leetcode 968 | Hard

---
+++ June LeetCoding Challenge 2022/Binary Tree Cameras.py
@@ -0,0 +1,30 @@
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        def postorder(node):
+            if not node:
+                return (0, math.inf)
+            
+            l_count, l_state = postorder(node.left)
+            r_count, r_state = postorder(node.right)
+            
+            state = min(l_state, r_state)
+            total_cameras = l_count + r_count
+            
+            if state==0: # children are not monitored
+                return (total_cameras + 1, 1) # install camera in current node
+            
+            if state==1: # one of the children is monitored and has camera
+                return (total_cameras, 2) # set current node state as monitored but no camera
+            
+            return (total_cameras, 0) 
+        dummy=TreeNode(-1, root) 
+        
+        return postorder(dummy)[0]
+      
