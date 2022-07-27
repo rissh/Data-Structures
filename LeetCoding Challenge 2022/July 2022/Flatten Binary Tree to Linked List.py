7da08bafb3a3bfd18181387876420c4cbc8d9ed2
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2022-07-27 21:43:50 +0530

    Leetcode 114 | Medium

---
+++ LeetCoding Challenge 2022/July 2022/Flatten Binary Tree to Linked List.py
@@ -0,0 +1,31 @@
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        
+        def dfs(node):
+            if not node:
+                return None,None
+            left,left2 = dfs(node.left)
+            right,right2 = dfs(node.right)
+            
+            node.left = None
+            end = node
+            if left:
+                end.right = left
+                end = left2
+            if right:
+                end.right = right
+                end = right2
+            return node,end
+        
+        return dfs(root)
+        
