977738738d18f4dfdfba6359f04050694428a102
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2022-11-06 23:26:57 +0530

    Leetcode 899 | Hard

---
+++ LeetCoding Challenge 2022/November 2022/Orderly Queue.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        
+        if k == 1:
+            ans = s
+            for _ in range(len(s)):
+                s = s[1:] + s[0]
+                ans = min(ans, s)
+            return ans
+        else:
+            return ''.join(sorted(s))
+          
