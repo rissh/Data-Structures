d0a4269914728753373aeb3145e7386d2cbed370
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2022-11-10 21:38:25 +0530

    Leetcode 1047 | Easy

---
+++ LeetCoding Challenge 2022/November 2022/Remove All Adjacent Duplicates In String.py
@@ -0,0 +1,30 @@
+# Method 1 : Using For Loop
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        
+        res=[]
+        for i in s:
+            if res and res[-1]==i:
+                res.pop()
+            else:
+                res.append(i)
+                
+        return "".join(res)        
+        
+# Method 2 : Using While Loop
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        
+        res = []
+        n = len(s)
+        i = 0
+        
+        while(i < n):
+            if res and res[-1] == s[i]:
+                res.pop()
+            else:
+                res.append(s[i])
+            i += 1
+            
+        return ''.join(res) 
+        
