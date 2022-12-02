253c0236edac3494627dd8566d5e2819c96e40a2
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2022-12-02 23:08:52 +0530

    Leetcode 1704 | Easy

---
+++ LeetCoding Challenge 2022/December/Determine if String Halves Are Alike.py
@@ -0,0 +1,25 @@
+
+
+def f(num):
+    
+    if(num == 'a' or num == 'e' or num == 'i' or num == 'o' or num == 'u' or num == 'A' or num == 'E' or num == 'I' or num == 'O' or num == 'U'):
+        return True
+    
+    return False
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        n = len(s)
+        count1 = 0
+        count2 = 0
+        
+        for i in range(n//2):
+            if(f(s[i])):
+                count1 += 1
+        
+        for i in range(n//2,n):
+            if(f(s[i])):
+                count2 += 1
+                
+        return count1 == count2     
+      
