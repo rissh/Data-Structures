cacf8df62bf079794f266acf20d0ce42a6bf3b68
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-01-09 18:35:50 +0530

    Leetcode 144 | Easy

---
+++ LeetCoding Challenge 2023/January ‍2023/Binary Tree Preorder Traversal.py
@@ -0,0 +1,18 @@
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.output = []
+        def traverse(root):
+            if(root):
+                self.output.append(root.val)
+                traverse(root.left)
+                traverse(root.right)
+        traverse(root)
+        return self.output
+        
