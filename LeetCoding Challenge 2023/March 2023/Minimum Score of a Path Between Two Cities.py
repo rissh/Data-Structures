fca2606425dcb04159b8e8dd69874c479c370a1a
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-03-23 00:53:39 +0530

    Leetcode 2492 | Medium

---
+++ LeetCoding Challenge 2023/March 2023/Minimum Score of a Path Between Two Cities.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+
+        adj=[[] for _ in range(n+1)]
+
+        for i,j,d in roads:
+            adj[i].append((d,j))
+            adj[j].append((d,i))
+        st=[1]
+        visited=[0]*(n+1)
+        visited[1]=1
+        mn=float("infinity")
+
+        while st:
+            x=st.pop(0)
+            for dt,i in adj[x]:
+                # print(dt,i)
+                if visited[i]==0:
+                    # print(dt)
+                    st.append(i)
+                    visited[i]=1
+                mn=min(mn,dt)
+                
+        return mn
+        
