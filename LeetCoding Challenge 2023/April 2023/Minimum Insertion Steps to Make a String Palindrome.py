3b2cf869f57c0556fdebf9c993f87b394fa80797
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-04-30 23:11:38 +0530

    Leetcode 1312 | Hard

---
+++ LeetCoding Challenge 2023/April 2023/Minimum Insertion Steps to Make a String Palindrome.py
@@ -0,0 +1,30 @@
+
+def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n = len(text1)
+        m = len(text2)
+        dp = [[-1 for i in range(m+1)] for i in range(n+1)]
+        
+        for j in range(m+1):
+            dp[0][j] = 0
+            
+        for i in range(n+1):
+            dp[i][0] = 0    
+            
+        for ind1 in range(1,n+1):
+            for ind2 in range(1,m+1):
+                if(text1[ind1-1] == text2[ind2-1]):
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1]
+                else:
+                    dp[ind1][ind2] = max(dp[ind1-1][ind2],dp[ind1][ind2-1])
+        return dp[n][m]
+
+
+def longestPalindromeSubseq(self, s: str) -> int:
+    t = s[::-1]
+    return longestCommonSubsequence(self,s,t)
+    
+class Solution:
+    def minInsertions(self, nums: str) -> int:
+        n = len(nums)
+        return n - longestPalindromeSubseq(self,nums)
+        
