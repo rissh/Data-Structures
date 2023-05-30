528f8f96149a3999a546384494f62e2b8bb65198
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-05-30 23:04:09 +0530

    Leetcode 399 | Medium

---
+++ LeetCoding Challenge 2023/May 2023/Evaluate Division.py
@@ -0,0 +1,30 @@
+
+class Solution:
+    # find path in graph equationValues
+    def findPath(self, A: str, B: str) -> float:
+        if A == B:
+            return 1 if A in self.equationValues.keys() else -1
+        
+        seen = set()
+        queue = [(A, 1)]
+        while queue:
+            Ai, value = queue.pop()
+            for Bi, valuei in self.equationValues[Ai].items():
+                if B == Bi:
+                    return value * valuei
+                else:
+                    if Bi not in seen:
+                        seen.add(Bi)
+                        queue.append((Bi, value * valuei))
+        return -1
+    
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        
+		# set up graph
+        self.equationValues = defaultdict(dict)
+        for (Ai, Bi), value in zip(equations, values):
+            self.equationValues[Ai][Bi] = value
+            self.equationValues[Bi][Ai] = 1 / value
+                
+        return [self.findPath(Cj, Dj) for (Cj, Dj) in queries]
+        
