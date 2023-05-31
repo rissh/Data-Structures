93a569f8191712fbc955edeb3e567ea928a418e5
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-05-31 22:50:54 +0530

    Leetcode 705 | Easy

---
+++ LeetCoding Challenge 2023/May 2023/Design HashSet.py
@@ -0,0 +1,24 @@
+
+class MyHashSet:
+
+    def __init__(self):
+        self.hs = 0
+    
+    def add(self, key: int) -> None:
+        if not self.contains(key):
+            self.hs += 1<<key
+    
+    def remove(self, key: int) -> None:
+        if self.contains(key):
+            self.hs -= 1<<key
+    
+    def contains(self, key: int) -> bool:
+        return (self.hs>>key)&1
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
