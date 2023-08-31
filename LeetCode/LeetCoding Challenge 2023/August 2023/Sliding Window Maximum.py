76cf0e08213939993dbd2ab5671ba937b631f24a
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-09-01 00:32:15 +0530

    Leetcode 239 | Medium

--- LeetCode/LeetCoding Challenge 2023/August 2023/Sliding Window Maximum.py
+++ LeetCode/LeetCoding Challenge 2023/August 2023/Sliding Window Maximum.py
@@ -1,1 +1,25 @@
 
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+
+        res = []
+        q = collections.deque()
+        l = r = 0
+
+        n = len(nums)
+
+        while r < n:
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r+1) >= k:
+                res.append(nums[q[0]])
+                l += 1
+            r += 1
+
+        return res
+      
