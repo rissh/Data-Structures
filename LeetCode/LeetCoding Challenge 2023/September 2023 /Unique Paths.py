fab8914623e9b42aeb27f873d61f0efe301c3827
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-09-05 23:36:22 +0530

    Leetcode 62 | Medium

---
+++ LeetCode/LeetCoding Challenge 2023/September 2023 /Unique Paths.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        prev = [0] * n
+        for i in range(0,m):
+            curr = [0] * n
+            for j in range(0,n):
+                
+                if(i==0 and j==0):
+                    curr[j] = 1
+                    
+                else:
+                    up,left = 0,0
+                    
+                    if(i>0):
+                        up = prev[j]
+                    if(j>0):
+                        
+                        left = curr[j-1]
+                    curr[j] = up+left
+                    
+            prev = [x for x in curr]        
+                    
+        return prev[n-1]
+      
