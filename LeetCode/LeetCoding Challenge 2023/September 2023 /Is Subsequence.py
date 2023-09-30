8a8cb52dfce9d2969e87a838850b110df3f85ab3
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-09-30 19:10:30 +0530

    Leetcode 392 | Easy

---
+++ LeetCode/LeetCoding Challenge 2023/September 2023 /Is Subsequence.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        if len(s) == 0: return True
+        if len(t) == 0: return False
+
+        index = 0
+        
+        for i in range(len(t)):
+            if s[index] == t[i]:
+                index += 1
+            if index == len(s): 
+                return True
+                
+        return False
+      
