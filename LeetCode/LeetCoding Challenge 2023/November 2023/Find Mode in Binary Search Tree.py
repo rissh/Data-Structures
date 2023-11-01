d360d204de4484060581c15f743da598678b2e05
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-11-01 23:43:12 +0530

    Leetcode 501 | Easy

---
+++ LeetCode/LeetCoding Challenge 2023/November 2023/Find Mode in Binary Search Tree.py
@@ -0,0 +1,33 @@
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.d = {}
+        self.m = 1
+        self.ans = []
+        
+    def dfs(self, x):
+        if not x:
+            return
+        if x.val not in self.d:
+            self.d[x.val] = 1    
+        else:
+            self.d[x.val] += 1
+            self.m = max(self.m, self.d[x.val])
+            
+        self.dfs(x.left)
+        self.dfs(x.right)
+    
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        self.dfs(root)
+        for x in self.d:
+            if self.d[x] == self.m:
+                self.ans.append(x)
+                
+        return self.ans
+      
