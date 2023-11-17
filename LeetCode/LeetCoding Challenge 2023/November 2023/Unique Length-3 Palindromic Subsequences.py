9fb95b69ea76eaf84b755b561c165aca8539b76a
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-11-17 23:45:50 +0530

    Leetcode 1930 | Medium

---
+++ LeetCode/LeetCoding Challenge 2023/November 2023/Unique Length-3 Palindromic Subsequences.py
@@ -0,0 +1,22 @@
+
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+
+        n = len(s)
+        res = set()
+        left = set()
+        right = collections.Counter(s)
+
+        for i in range(n):
+            right[s[i]] -= 1
+            if right[s[i]] == 0:
+                right.pop(s[i])
+
+            for j in range(26):
+                c = chr(ord("a") + j)
+                if c in left and c in right:
+                    res.add((s[i], c))
+            left.add(s[i])
+
+        return len(res)
+      
