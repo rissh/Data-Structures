ebb4685c115a529d1c3c8d6e3f389060b180c4c5
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-12-26 23:51:59 +0530

    Leetcode 867 | Easy

---
+++ LeetCode/LeetCoding Challenge 2023/December 2023/Transpose Matrix.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        res = [[0 for i in range(m)] for i in range(n)]
+        
+        for i in range(m):
+            for j in range(n):
+                res[j][i] = matrix[i][j]
+                
+        return res 
