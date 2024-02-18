de0bf2ee310a5062d01758cad8b3cf523f6e1246
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2024-02-18 22:55:21 +0530

    Leetcode | Medium

---
+++ LeetCode/Leetcode Contests/Weekly Contest 385/Find the Length of the Longest Common Prefix.py
@@ -0,0 +1,33 @@
+
+class Trie:
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, num):
+        node = self.root
+        for char in str(num):
+            node = node.setdefault(char, {})
+        node['#'] = True
+
+    def f(self, num):
+        node, res = self.root, 0
+        
+        for char in str(num):
+            if char not in node:
+                return res
+            node, res = node[char], res + 1
+            
+        return res
+    
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+
+        trie, maxLen = Trie(), 0
+        
+        for num in arr1:
+            trie.insert(num)
+        for num in arr2:
+            maxLen = max(maxLen, trie.f(num))
+            
+        return maxLen
+      
