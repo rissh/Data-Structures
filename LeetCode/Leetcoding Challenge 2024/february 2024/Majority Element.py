15f92180c12ea6370bafa6bb160b004c5c8a7f5e
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2024-02-29 22:56:13 +0530

    Leetcode 169 | Easy

---
+++ LeetCode/Leetcoding Challenge 2024/february 2024/Majority Element.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        count = 0
+        curr = 0
+        
+        for i in range(len(nums)):
+            if(count == 0):
+                curr = nums[i]
+            if(curr == nums[i]):
+                count += 1
+            else:
+                count -= 1
+                
+        return curr   
