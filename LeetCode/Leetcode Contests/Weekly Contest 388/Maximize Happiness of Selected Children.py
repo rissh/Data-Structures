4801f3908a581c277e82be9060323f2e15f1693d
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2024-03-13 22:27:19 +0530

    Leetcode Weekly Contest 388

---
+++ LeetCode/Leetcode Contests/Weekly Contest 388/Maximize Happiness of Selected Children.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        
+        n = len(happiness)
+        happiness.sort(reverse=True)
+        counter = 0
+        res = 0
+        
+        for i in range(len(happiness)):
+            res += max(0, happiness[i] - counter)
+            counter += 1
+            
+            if counter >= k:
+                break
+                
+        return res
+      
