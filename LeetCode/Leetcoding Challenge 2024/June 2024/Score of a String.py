023b6736dd88e69a50e74bfd575977933c52de03
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2024-06-05 23:39:41 +0530

    Leetcode 3110 | Easy

---
+++ LeetCode/Leetcoding Challenge 2024/June 2024/Score of a String.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        
+        n = len(s)
+        res = 0
+        
+        for i in range(1, n):
+            
+            res += abs(ord(s[i]) - ord(s[i-1]))
+            
+        return res
+        
