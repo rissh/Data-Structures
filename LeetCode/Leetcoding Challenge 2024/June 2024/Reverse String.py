cf574d6dab1006af9b4b655f5f4a491c87ce4830
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2024-06-06 23:50:28 +0530

    Leetcode 344 | Esay

---
+++ LeetCode/Leetcoding Challenge 2024/June 2024/Reverse String.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        #s = s.reverse()
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
+      
