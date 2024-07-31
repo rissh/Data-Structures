2d5df6d678a3dee3598280daf070099a448116e7
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2024-08-01 00:30:32 +0530

    Leetcode

---
+++ LeetCode/Leetcoding Challenge 2024/July 2024/Crawler Log Folder.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+
+        res = 0
+
+        for log in logs:
+            if log == "./":
+                continue
+
+            elif log == "../":
+                res = max(0, res - 1)
+                #if res > 0:
+                #    res -= 1
+
+            else:
+                res += 1
+        return res
+        
