eddbaceed13a80a2fcd43b69d97b88ffbb0dd4d4
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2024-08-01 00:29:01 +0530

    Leetcode | Easy

---
+++ LeetCode/Leetcoding Challenge 2024/July 2024/Water Bottles.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+
+        res = 0
+        empty = 0
+        while numBottles > 0:
+            res += numBottles
+            empty += numBottles
+            numBottles = empty // numExchange
+            empty = empty % numExchange
+
+        return res
+        
