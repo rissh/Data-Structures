b1cce6dac0eeea1b498dfd14ced07f1163ef9f8e
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2024-09-30 22:21:25 +0530

    Leetcode 179 | Medium

---
+++ LeetCode/Leetcoding Challenge 2024/September 2024/Largest Number.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        for i, n in enumerate(nums):
+            nums[i] = str(n)
+        
+        def compare(x, y):
+            if x + y > y + x:
+                return -1  
+            else:
+                return 1   
+              
+        nums = sorted(nums, key=cmp_to_key(compare))
+        return str(int("".join(nums)))
+        
